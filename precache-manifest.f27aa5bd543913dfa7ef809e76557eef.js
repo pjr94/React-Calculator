6bbe319f7a62742a6ee4346497b48a4119b4fe0d
pr <pjr94@users.noreply.github.com>
2020-04-17 12:47:10 +0100

    Updates

---
+++ precache-manifest.f27aa5bd543913dfa7ef809e76557eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947926b6e15acc337baf422abce30a44",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "4457845fe0e19e5c8859",
+    "url": "/React-Calculator/static/css/main.02da65ff.chunk.css"
+  },
+  {
+    "revision": "7c3b248bc5222e7bf2e3",
+    "url": "/React-Calculator/static/js/2.86d35a27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.86d35a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4457845fe0e19e5c8859",
+    "url": "/React-Calculator/static/js/main.2c20e68c.chunk.js"
+  },
+  {
+    "revision": "c6402b928dc7bf31af59",
+    "url": "/React-Calculator/static/js/runtime-main.69252beb.js"
+  }
+]);
